1c922c4686b7341390837c225ab8fb7d85319e6f
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-29 12:22:14 +0000

    Update dois.php, quatro.php, and tres.php

---
+++ dois.php
@@ -0,0 +1,7 @@
+<?php 
+
+$numero = rand(1,10)
+for ($i = 1 ; $i <= $numero ;  $i++) { 
+    print($numero)
+}
+?>
---
+++ tres.php
@@ -0,0 +1,10 @@
+<?php 
+
+$numero1 = rand(1,10);
+$i = 1;
+while ($i <= 10) {
+	$res = $numero1 = $i;
+	echo $i . ' x ' . $numero1. ' = ' . $res . ' <br> ';
+	$i++;
+}
+?>
